04e585c3e845d57debd9fe24ac83f86745d3b12d
Jorge Rodriguez <jorge.rodriguez@guayama.inter.edu>
2023-02-15 08:25:39 -0400

    Initial commit

---
+++ app/src/main/java/com/example/areaapp2023/MainActivity.kt
@@ -0,0 +1,34 @@
+package com.example.areaapp2023
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.EditText
+import android.widget.TextView
+import androidx.core.widget.doAfterTextChanged
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var widthWidget: EditText
+    private lateinit var heightWidget: EditText
+    private lateinit var resultWidget: TextView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        widthWidget = findViewById(R.id.widthEditText)
+        heightWidget = findViewById(R.id.heightEditText)
+        resultWidget = findViewById(R.id.resultTextView)
+
+        widthWidget.doAfterTextChanged {
+            calculateArea()
+        }
+
+        heightWidget.doAfterTextChanged {
+            calculateArea()
+        }
+    }
+
+    fun calculateArea() {
+
+    }
+}
